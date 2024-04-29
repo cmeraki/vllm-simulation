892594368596eac9365a52156373697f4598a89b
cmeraki <compute@merakilabs.com>
2024-04-29 17:45:33 +0530

    Final version of README and requirements

--- README.md
+++ README.md
@@ -2,15 +2,25 @@
 
 This is the accompanying repo to the blog [Throughput is all you need](https://cmeraki.github.io/blogs/throughput.html). The code here simulates a chat application, where a user engages with an LLM powered bot in a multi-turn conversation.
 
+Here, we have used a flavor of Mistral 7B model developed by Teknium. You can find more information about them [here](https://huggingface.co/teknium).
+
 ## Setup
+
+### Prerequisites
+
+You need to have the following tools setup in your system
+
+1. git
+2. Docker CLI and Docker compose
+3. Python 3
 
 ### Step 0: Setup the environment
 
 ```bash
 git clone https://github.com/cmeraki/vllm-simulation.git
 cd vllm-simulation
-python -m venv myenv
-source myenv/bin/activate
+python -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
 ```
 
@@ -18,30 +28,29 @@ pip install -r requirements.txt
 
 To replicate the experiments from the blog, we would need three running processes.
 
-1. vLLM serving an LLM on an OpenAI-compatible server. After installing the setup, run this command in a terminal window:
+1. vLLM serving an LLM on an OpenAI-compatible server. After running step 0, run this command in a new terminal window:
 
 ```bash
-python3 <>
+python -m vllm.entrypoints.openai.api_server --model teknium/OpenHermes-2.5-Mistral-7B --max-model-len 8192 --disable-log-requests
 ```
 
-2. Monitoring setup: To set up the monitoring, follow the steps mentioned [here](https://github.com/vllm-project/vllm/tree/main/examples/production_monitoring). This will be important to visualize the metrics.
-3. Simulation: To finally run the simulation, open a terminal window in this repository and run the following command
+2. Monitoring setup: To set up the monitoring, follow the steps mentioned [here](https://github.com/vllm-project/vllm/tree/main/examples/production_monitoring). This will be important to visualize the metrics. To run the code in the given link above, you can either download the code or clone the repository itself.
+
+3. Simulation: To finally run the simulation, open a new terminal window in this repository location and run the following command
 
 ```bash
-python --model_id togethercomputer/Llama-2-7B-32K-Instruct \
-    -r 0.1 -n 100 -l 9 -u 11 \
-    simulation.py
+python simulation.py --model teknium/OpenHermes-2.5-Mistral-7B -n 50 -l 10 -u 11
 ```
 
-You can customize the following arguments to the script.
+You can customize the following arguments to the above script.
 
 ```bash
 python simulation.py -h
-usage: simulation.py [-h] [--model_id MODEL_ID] [--uri URI] [--port PORT] [-r R] [-n N] [-l L] [-u U]
+usage: simulation.py [-h] [--model MODEL] [--uri URI] [--port PORT] [-r R] [-n N] [-l L] [-u U]
 
 options:
   -h, --help           show this help message and exit
-  --model_id MODEL_ID  Model name that is called for inference
+  --model MODEL        Model name that is called for inference
   --uri URI            URI where the model is available for inference
   --port PORT          Port where the model is available for inference
   -r R                 Number of requests per second
@@ -49,3 +58,9 @@ options:
   -l L                 Lower bound of conversations in a single chat
   -u U                 Upper bound of conversations in a single chat
 ```
+
+Given that you have run all the steps successfully, you would be able to visualize the metrics at http://localhost.com:3000.
+
+## Appendix
+
+The simulation was run on an Nvidia RTX 4090 GPU.
--- simulation.py
+++ simulation.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from loguru import logger
 from llm import LLM
 
-sys_msg = 'You are a human user interacting with a conversational AI assistant or chatbot. You will engage in natural conversations, asking questions, making requests, and responding just as you would when talking to a real AI assistant. You can ask about any topics, request assistance with tasks, or just have an open-ended conversation. The key is to role-play as a typical user would when conversing with an AI chatbot, speaking in a casual, conversational manner and making your queries and responses sound like they would come from a human, not an AI itself. Do not break character or give any indication that you are an AI language model. Simply act as a realistic human user would when chatting with an AI assistant.'
+sys_msg = 'You are a human user interacting with a conversational AI assistant or chatbot. You will engage in natural conversations, asking questions, making requests, and responding just as you would when talking to a real AI assistant. You can ask about any topics, request assistance with tasks, or just have an open-ended conversation. The key is to role-play as a typical user would when conversing with an AI chatbot, speaking in a casual, conversational manner and making your queries and responses sound like they would come from a human, not an AI itself. Do not break character or give any indication that you are an AI language model. Simply act as a realistic human user would when chatting with an AI assistant. Reply in no more than 500 words'
 
 sys_prompt = {
     'role': 'system',
@@ -38,12 +38,12 @@ def user_llm() -> str:
     return ds[idx]['prompt']
 
 
-def orchestrate_conversation(model_id: str, uri: str, conversation_id: int, num_total_turns: int) -> None:
+def orchestrate_conversation(model: str, uri: str, conversation_id: int, num_total_turns: int) -> None:
     """
     Starts a chat between a simulated user and an LLM
 
     Args
-        - model_id {str}:           Name of the model to call
+        - model {str}:           Name of the model to call
         - uri {str}:                URI of the OpenAI compatible server
         - conversation_id {int}:    A unique identifier for the conversation
         - num_total_turns {int}:    Number of total turns to simulate the chat
@@ -54,7 +54,7 @@ def orchestrate_conversation(model_id: str, uri: str, conversation_id: int, num_
     logger.bind(conversation_id=conversation_id).info(f'Starting conversation with conversation id: {conversation_id}')
 
     chatbot_llm = LLM(
-        model_id=model_id,
+        model_id=model,
         uri=uri,
         messages=[]
     )
@@ -104,30 +104,33 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--model_id', type=str, required=False, default='David-Xu/Mistral-7B-Instruct-v0.2', help='Model name that is called for inference')
+    parser.add_argument('--model', type=str, required=False, default='David-Xu/Mistral-7B-Instruct-v0.2', help='Model name that is called for inference')
     parser.add_argument('--uri', type=str, required=False, default='localhost', help='URI where the model is available for inference')
     parser.add_argument('--port', type=str, required=False, default='8000', help='Port where the model is available for inference')
-    parser.add_argument('-r', type=float, required=False, default=4, help='Number of requests per second')
-    parser.add_argument('-n', type=int, required=False, default=20, help='Time unit to run the test for')
+    parser.add_argument('-r', type=float, required=False, default=None, help='Number of requests per second')
+    parser.add_argument('-n', type=int, required=False, default=20, help='Number of requests to run')
     parser.add_argument('-l', type=int, required=False, default=15, help='Lower bound of conversations in a single chat')
     parser.add_argument('-u', type=int, required=False, default=25, help='Upper bound of conversations in a single chat')
 
     args = parser.parse_args()
-    model_id = args.model_id
+    model = args.model
     complete_uri = f'http://{args.uri}:{args.port}/v1'
     rate = args.r
     time_unit = args.n
     l, u = args.l, args.u
 
-    assert rate <= 1, f"Rate should be less than equal to 1. Provided: {rate}"
+    if rate:
+        assert rate <= 1, f"Rate should be less than equal to 1. Provided: {rate}"
 
-    logger.info(f'Model id: {model_id}, model uri: {complete_uri}')
+    logger.info(f'Model id: {model}, model uri: {complete_uri}')
     logger.info(f'Rate: {rate}, time units: {time_unit}')
     logger.info(f'Lower bound of total turns: {l}, higher bound of total turns: {u}')
 
     # Uniformly sample arrival times so that the mean is closer to the provided rate
     # This can also be replaced with sampling form poisson distribution to align better with the real world
-    arrival_rates = np.random.randint(int(1/rate), int(1/rate)+1, time_unit)
+    arrival_rates = np.zeros(time_unit)
+    if rate:
+        arrival_rates = np.random.randint(int(1/rate), int(1/rate)+1, time_unit)
     num_turns = np.random.randint(l, u, time_unit)
 
     logger.info(f'Arrvial rates: {arrival_rates}, num turns: {num_turns}')
@@ -137,9 +140,10 @@ if __name__ == '__main__':
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for arrival, num_total_turns in zip(arrival_rates, num_turns):
             logger.info(f'Sleeping for {arrival}s')
-            time.sleep(arrival)
+            if rate:
+                time.sleep(arrival)
 
-            f = partial(orchestrate_conversation, model_id=model_id, uri=complete_uri, conversation_id=idx, num_total_turns=num_total_turns)
+            f = partial(orchestrate_conversation, model=model, uri=complete_uri, conversation_id=idx, num_total_turns=num_total_turns)
             executor.submit(f)
             idx += 1
 
